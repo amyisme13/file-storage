1d4de9509633489018c32ac2f8455b7757d95a63
Azmi Makarima <amy.azmim@gmail.com>
2021-02-21 19:05:00 +0700

    create a custom S3 client

---
+++ app/Support/Aws/S3Client.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Support\Aws;
+
+use Illuminate\Support\Facades\Storage;
+
+class S3Client
+{
+    /**
+     * @var string
+     */
+    private $disk = 's3';
+
+    /**
+     * Set the disk used to get the adapter & client.
+     *
+     * @param string $disk
+     * @return $this
+     */
+    public function setAdapter($disk)
+    {
+        $this->disk = $disk;
+
+        return $this;
+    }
+
+    /**
+     * Get a presigned url for the file at the given path.
+     *
+     * @param string $path
+     * @param \DateTimeInterface $expiration
+     * @param array $options
+     * @param string $commandName
+     * @return string
+     */
+    public function getPresignedUrl(
+        $path,
+        $expiration,
+        $options,
+        $commandName = 'GetObject'
+    ) {
+        /** @var \League\Flysystem\AwsS3v3\AwsS3Adapter $adapter */
+        $adapter = Storage::disk($this->disk)->getAdapter();
+
+        $client = $adapter->getClient();
+        $command = $client->getCommand(
+            $commandName,
+            array_merge(
+                [
+                    'Bucket' => $adapter->getBucket(),
+                    'Key' => $adapter->getPathPrefix() . $path,
+                ],
+                $options
+            )
+        );
+
+        return (string) $client
+            ->createPresignedRequest($command, $expiration)
+            ->getUri();
+    }
+}
