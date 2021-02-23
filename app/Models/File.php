ce19d22f1d6d0999e3bde09d3c09a1b41c642586
Azmi Makarima <amy.azmim@gmail.com>
2021-02-23 17:34:23 +0700

    add soft deletes & user relation to File

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,13 +5,15 @@ namespace App\Models;
 use App\Support\Aws\S3Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\File as FacadesFile;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
 class File extends Model
 {
-    use HasFactory, HasSlug;
+    use HasFactory, HasSlug, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +21,27 @@ class File extends Model
      * @var array
      */
     protected $fillable = ['name', 'type', 'size'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['id', 'job_id', 'user_id'];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['user'];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['url', 'thumbnail_url'];
 
     /**
      * Get the route key for the model.
@@ -74,5 +97,46 @@ class File extends Model
     public function getUploadedAttribute()
     {
         return !is_null($this->uploaded_at);
+    }
+
+    /**
+     * Get the url from path
+     *
+     * @return string|null
+     */
+    public function getUrlAttribute()
+    {
+        if (is_null($this->path)) {
+            return null;
+        }
+
+        return Storage::disk('s3')->temporaryUrl($this->path, now()->addHour());
+    }
+
+    /**
+     * Get the thumbnail url from thumbnail
+     *
+     * @return string|null
+     */
+    public function getThumbnailUrlAttribute()
+    {
+        if (is_null($this->thumbnail)) {
+            return null;
+        }
+
+        return Storage::disk('s3')->temporaryUrl(
+            $this->thumbnail,
+            now()->addHour()
+        );
+    }
+
+    /**
+     * Return the user that upload the file.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
