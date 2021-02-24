22415bcd23da1dabc39178e5fa0808c59d76227e
Azmi Makarima <amy.azmim@gmail.com>
2021-02-24 23:41:46 +0700

    fix HandleSnsNotification

--- app/Listeners/HandleSnsNotification.php
+++ app/Listeners/HandleSnsNotification.php
@@ -3,16 +3,18 @@
 namespace App\Listeners;
 
 use App\Models\SnsNotification;
+use Illuminate\Support\Carbon;
+use Rennokki\LaravelSnsEvents\Events\SnsNotification as EventsSnsNotification;
 
 class HandleSnsNotification
 {
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param \Rennokki\LaravelSnsEvents\Events\SnsNotification $event
      * @return void
      */
-    public function handle($event)
+    public function handle(EventsSnsNotification $event)
     {
         $message = $event->payload['message'];
 
@@ -20,7 +22,7 @@ class HandleSnsNotification
         $notification->message_id = $message['MessageId'];
         $notification->message = $message['Message'];
         $notification->topic_arn = $message['TopicArn'];
-        $notification->message_timestamp = $message['Timestamp'];
+        $notification->message_timestamp = Carbon::parse($message['Timestamp']);
         $notification->raw = json_encode($message);
         $notification->save();
     }
